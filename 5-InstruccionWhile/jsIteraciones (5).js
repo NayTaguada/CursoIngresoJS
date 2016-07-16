b72350c654b24142d9c39a0b39ec024fbd661e7a
nayTaguada <naylalucia@gmail.com>
2016-07-15 22:04:17 -0300

    Signed-off-by: nayTaguada <naylalucia@gmail.com>

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,11 +3,22 @@ function Mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+	if(sexo == "f" || sexo == "m"){
+			alert("Su sexo es: " + sexo);
+		} else {
+
 	while(sexo != "f" || sexo != "m"){
 
 		sexo = prompt("Ingrese nuevamente el sexo, f o m");
 
+		if(sexo == "f" || sexo == "m"){
+			alert("Su sexo es: " + sexo);
+			break;
+		} 
+		
 	}
+
+}
 
 document.getElementById('Sexo').value=sexo;
 
