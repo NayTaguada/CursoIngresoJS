30277ceaa0159ddb785403758b7b91e5f0828055
NayTaguada <naylalucia@gmail.com>
2016-07-27 21:52:50 -0300

    TP, isNaN
    Signed-off-by: NayTaguada <naylalucia@gmail.com>

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -11,6 +11,12 @@ function Mostrar()
 
 		numero = prompt("Ingrese el numero: " + contador);
 
+		while(isNaN(numero) == true){     		// En los parentesis se pone el valor a comparar, cuando es true, no es un numero. False, si lo es.
+
+			numero = prompt("Ingrese el numero: " + contador);
+			//continue;  // Para que al ingresar un correcto numero, siga pidiendo los demas.
+		}
+
 		acumulador = acumulador + parseInt(numero);
 
 	}
--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,70 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+ 	var edad = prompt("Ingrese una edad entre 18 y 90 años");
+
+ 		while(isNaN(edad)){
+ 		edad = prompt("Ingrese una edad en formato de numeros");
+ 		}
+
+			while(edad<18 || edad>90){
+ 			edad = prompt("Ingrese una edad valida");
+ 			}
+
+ 	var sexo = prompt("Ingrese sexo, F o M");
+
+ 		while(sexo != "M" && sexo != "F"){
+ 			sexo = prompt("Ingrese M si es masculino o F si es femenino");
+ 			break;
+ 		}
+
+ 	var estadoCivil = prompt("Ingrese estado civil");
+
+ 		while(isNaN(estadoCivil)){
+ 		estadoCivil = prompt("Ingrese estado civil, utilizando 1, 2, 3 o 4");
+ 		}
+
+ 		while(estadoCivil>= 1 || estadoCivil<5){
+
+ 			switch(estadoCivil){
+
+ 				case 1:
+ 				estadoCivil = "Soltero";
+ 				break;
+
+	 			case 2: 
+	 			estadoCivil = "Casado";
+	 			break;
+
+	 			case 3: 
+	 			estadoCivil = "Divorciado"
+	 			break;
+
+	 			case 4:
+	 			estadoCivil = "Viudo";
+	 			break;
+
+ 			}
+
+ 		}
+
+ 	var sueldoBruto = prompt("Ingrese sueldo bruto, mayor a 8000");
+ 	var nLegajo = prompt("Ingrese numero de legajo");
+ 	var nacionalidad = prompt("Ingrese nacionalidad, siendo A para argentinos, E para extranjeros y N para nacionalizados");
+
+
+
+
+
+
+
+
+
+ 	document.getElementById('Edad').value = edad;
+ 	document.getElementById('Sexo').value = sexo;
+ 	document.getElementById('EstadoCivil').value = estadoCivil;
+ 	document.getElementById('Sueldo').value = sueldoBruto;
+ 	document.getElementById('Legajo').value = nLegajo;
+ 	document.getElementById('Nacionalidad').value = nacionalidad;
+
 }
